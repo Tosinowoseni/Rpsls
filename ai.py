9ac94431679de5529240374b93cba1f53caa21e8
tosinowoseni <tosinowosen1@icloud.com>
2022-04-10 17:29:16 -0400

    Created All Files for Rpsls Project + Created Ai.py File

---
+++ ai.py
@@ -0,0 +1,15 @@
+from player import player
+import random
+from time import sleep
+class AI(Player):
+
+    def __init__(self, name):
+        super().__init__()
+        self.score = 0
+        self.name = name
+
+    def choose_gesture(self):
+        self.chosen_gesture = str(random.randit(0,4))
+        gesture_list = ["Rock","Paper","Scissors","Lizard","Spock"]
+        sleep(1)
+        print(f"{self.name} has picked {gesture_list[int(self.chosen_gesture)]}")
